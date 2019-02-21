4431f58f4b0f7325516eeaf85d73085c43071fea
Ogun <omt66@yahoo.com>
2019-02-20 20:57:06 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  author: 'OMT', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://otitek.com',
+    github: 'https://github.com/omt66',
+    twitter: 'https://twitter.com/omt66',
+    linkedin: 'https://ca.linkedin.com/in/',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://odevblog.netlify.com/',
 };
